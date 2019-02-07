9ce92b001cc38e7537c126dd7714e2e9792e180f
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2019-02-07 14:41:50 -0800

    Add python scripts

---
+++ scripts/filter-pyNAST-for-ROI.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from Bio import SeqIO
+
+import argparse
+
+parser = argparse.ArgumentParser(description='This script filters a pyNAST alignment for a region of interest (ROI) specified by the user (i.e. a primer region in E. coli coordinates). It will output a fasta file containing only those records that do not contain gaps or ambiguous bases in a specific region of interest in a multiple sequence alignment. This will only work for pyNAST-like formats, which align to a fixed reference sequence.')
+
+parser.add_argument('--input', help='Your multiple sequence alignment (MSA) in gapped fasta format as produced by PyNAST.')
+
+parser.add_argument('--output', help='An output name for the cleaned MSA with only reads covering the region of interest.')
+
+parser.add_argument('--start', help='Start of the region of interest in E. coli 16S rRNA coordinates')
+
+parser.add_argument('--end', help='End of the region of interest in E. coli 16S rRNA coordinates')
+
+parser.add_argument('--padding',help='Number of padding bases required on edges of ROI before and after the region you specify so that the region is not right at the edge of the read. Default: 5 bases.',default='5')
+
+args = parser.parse_args()
+
+hashCleanSeqs = {}
+
+iStart = int(args.start) - int(args.padding)
+iEnd = int(args.end) + int(args.padding)
+
+for record in SeqIO.parse(args.input, "fasta"):
+
+	sequence = str(record.seq).upper()
+
+	if ("-" not in sequence[iStart:iEnd]) and ("N" not in sequence[iStart:iEnd]):
+	
+		hashCleanSeqs[record.id] = sequence
+	
+with open(args.output, "w+") as output_file:
+
+	for key, value in hashCleanSeqs.items():
+
+		output_file.write(">" + key + "\n" + value + "\n")
---
+++ scripts/grab-primer-region-in-fastq.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+"""
+Code taken from user "Jon" on biostars, see: https://www.biostars.org/p/163928/
+Also:
+https://github.com/nextgenusfs/amptk
+"""
+
+import re
+import csv
+import sys
+from Bio import SeqIO
+import argparse
+
+parser = argparse.ArgumentParser(description='This script grabs a very small region of interest from a fastq file that corresponds to part of the 16s/18s gene identified using graftM (HMMSEARCH) and aligned to the appropriate SSU reference sequence with pyNAST.')
+
+parser.add_argument('--strand', help='A tab-separated file with fastq identifiers and strand inferred by HMMSEARCH.')
+
+parser.add_argument('--lenROI', help='The length of your region of interest. E.g. the number of bases of the primer.')
+
+parser.add_argument('--padding', help='The number of bases to pad the resulting fastq slice on either end.')
+
+parser.add_argument('--start', help='Start of the region of interest in SSU rRNA coordinates appropriate to the reference')
+
+parser.add_argument('--fasta', help='Your fasta file containing the sequences to be sliced.')
+
+parser.add_argument('--fastq', help='Your fastq file containing the sequences to be sliced.')
+
+args = parser.parse_args()
+
+hashStrand = {}
+
+#create a dictionary with the strand information to later reverse complement fastqs as necessary
+for astrLine in csv.reader(open(args.strand), csv.excel_tab):
+
+	hashStrand[astrLine[0].strip()] = astrLine[1].strip()
+
+iROILength = int(args.lenROI)
+
+iPadding = int(args.padding)
+
+iStart = int(args.start) - 1 - iPadding
+
+hashCoordinates = {}
+
+#parse input fasta file to determine the number of actual nucleotides before the start site (i.e. ignoring gaps)
+for record in SeqIO.parse(args.fasta, "fasta"):
+
+	strSequenceSlice = str(record.seq).upper()[:iStart]
+
+	iLeadingBases = len(re.findall("[ACTG]", strSequenceSlice))
+
+	hashCoordinates[record.id] = iLeadingBases
+
+#use the information determined above to slice the fastq file to the primer region, taking into account strandedness
+for record in SeqIO.parse(args.fastq, "fastq"):
+
+	if record.id in hashCoordinates.keys(): #To account for those culled from the alignment
+
+		iPrimerBeg = hashCoordinates[record.id]
+
+		iPrimerEnd = iPrimerBeg + iROILength + (iPadding * 2)
+
+		if hashStrand[record.id] == "+":
+
+			PrimerRegion = record[iPrimerBeg:iPrimerEnd]
+
+			sys.stdout.write(PrimerRegion.format('fastq'))
+
+		elif hashStrand[record.id] == "-":
+
+			PrimerRegion = record.reverse_complement(id=True, name=True, description=True)[iPrimerBeg:iPrimerEnd]
+
+			sys.stdout.write(PrimerRegion.format('fastq'))
