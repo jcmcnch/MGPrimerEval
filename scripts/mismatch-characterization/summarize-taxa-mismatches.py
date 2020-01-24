5f19750006d0b7191888518c87e222a78f20fdd2
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2020-01-24 12:46:07 -0800

    add missing scripts

---
+++ scripts/mismatch-characterization/summarize-taxa-mismatches.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import csv
+
+headerCheck=True
+
+print('\t'.join(["Taxon","Frac mismatched at 0-mismatches","Frac mismatched at 1-mismatches","Frac mismatched at 2-mismatches"]))
+
+for astrLine in csv.reader(open(sys.argv[1]), csv.excel_tab):
+    if headerCheck:
+        headerCheck=False
+        continue
+    if not headerCheck:
+        if float(astrLine[3]) > 10 and float(astrLine[10]) > 10 and float(astrLine[17]) > 10 and float(astrLine[6]) > 0.05: #if at least 10 detected sequences found across all mismatches AND more than 5% mismatched
+            print('\t'.join([astrLine[0],astrLine[6],astrLine[13],astrLine[20]]))
