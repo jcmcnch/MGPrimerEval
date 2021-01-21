4c999637765dfd32e6cd5006df13a92ab46d4a7f
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2021-01-20 18:10:59 -0800

    update tutorial

--- tutorial/download-BGT.sh
+++ tutorial/download-BGT.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+#this won't work unless you have the following repository:
+#https://github.com/wwood/ena-fast-download
+#AND ascp:
+#https://download.asperasoft.com/download/docs/ascp/3.5.2/html/index.html
+
 while read line ; do
 
 	./ena-fast-download/ena-fast-download.py $line --ascp-args '-m 1m -l 100m'
-        mv *gz compute-workflow-intermediate/00-fastq/
+        mv *gz intermediate/compute-workflow-intermediate/00-fastq/
 
-done < tutorial/accessions/GA03-stn 
+done < tutorial/accessions/GA03-KN204-stn20.sra.ids 
