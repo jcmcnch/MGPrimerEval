e4f0e081623a7f552ba6ff45485786dba0f7b8c3
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2021-02-02 15:48:00 -0800

    add helper flag to tutorial runscript

--- run_tutorial.sh
+++ run_tutorial.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -i
 conda activate snakemake-env
-snakemake --cores 80 --use-conda --snakefile Snakefile-compute.smk --configfile config/tutorial/config.yaml
+snakemake --cores 80 --use-conda --snakefile Snakefile-compute.smk --configfile config/tutorial/config.yaml #--conda-create-envs-only #Uncomment out if you want to test conda's ability to set up environments properly
