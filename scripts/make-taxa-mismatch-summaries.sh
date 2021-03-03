886ae17ce035d2dd511b6944347b2892c59bac94
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2021-03-02 21:41:29 -0800

    update taxa plotting script

--- scripts/make-taxa-mismatch-summaries.sh
+++ scripts/make-taxa-mismatch-summaries.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -i
 
-#any old pandas-containing environment will do
-conda activate opedia-env
+#any old pandas-containing environment will do, this is the biopython environment from the conda recipe
+conda activate .snakemake/conda/a48b2588 
 
 #cleanup previous failed runs, if files exist
-rm classify-workflow-intermediate/11-taxa-with-many-mismatches/*info
-rm classify-workflow-intermediate/11-taxa-with-many-mismatches/*ids
+rm intermediate/classify-workflow/11-taxa-with-many-mismatches/*info
+rm intermediate/classify-workflow/11-taxa-with-many-mismatches/*ids
 rm -rf output-classify-workflow/taxa-mismatch-summaries
 
 mkdir -p output-classify-workflow/taxa-mismatch-summaries
@@ -14,20 +14,20 @@ for primer in 926R 806RB 515Y V4F V4R V4RB 341F 785R 27F 1389F 1510R ; do
 
 	for group in ARCH BACT-CYANO BACT-NON-CYANO EUK ; do
 
-		for taxon in `cat classify-workflow-intermediate/11-taxa-with-many-mismatches/*.$group.$primer.0-mismatch.gt50mm-and-5pcmismatched-taxa.txt`; do
+		for taxon in `cat intermediate/classify-workflow/11-taxa-with-many-mismatches/*.$group.$primer.0-mismatch.gt50mm-and-5pcmismatched-taxa.txt`; do
 
 			cleantaxon=`echo $taxon | sed 's/://g' | sed 's/,/-/g'`
-			input=`ls classify-workflow-intermediate/11-taxa-with-many-mismatches/*.$group.$primer.0-mismatch.gt50mm-and-5pcmismatched-taxa.headers.tsv`
+			input=`ls intermediate/classify-workflow/11-taxa-with-many-mismatches/*.$group.$primer.0-mismatch.gt50mm-and-5pcmismatched-taxa.headers.tsv`
 			study=`basename $input | cut -f1 -d\.`
-			output=classify-workflow-intermediate/11-taxa-with-many-mismatches/$study.$group.$primer.$cleantaxon.mismatched.ids
-			info=classify-workflow-intermediate/11-taxa-with-many-mismatches/$study.$group.$primer.$cleantaxon.mismatched.info
+			output=intermediate/classify-workflow/11-taxa-with-many-mismatches/$study.$group.$primer.$cleantaxon.mismatched.ids
+			info=intermediate/classify-workflow/11-taxa-with-many-mismatches/$study.$group.$primer.$cleantaxon.mismatched.info
 		
 			grep "$taxon" $input | cut -f1 > $output
 			
 			#add loop because grep -f takes forever and eats all of kraken's RAM!
 			while read line ; do
 
-				grep "$line" classify-workflow-intermediate/10-concatenated-info-files/*.$group.$primer.2-mismatch.nohits.all.info >> $info
+				grep "$line" intermediate/classify-workflow/10-concatenated-info-files/*.$group.$primer.2-mismatch.nohits.all.info >> $info
 
 			done < $output
 
