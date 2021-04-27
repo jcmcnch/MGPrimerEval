64de0e4aa6b40df6c4b40e7a7a8ed2099ebd4723
Inigo Montoya <inigo@kraken.usc.edu>
2021-04-27 14:24:48 -0700

    Update setup script from test account (Inigo)

--- setup_env_db.sh
+++ setup_env_db.sh
@@ -1,2 +1,7 @@
 #!/bin/bash -i
-
+#Install miniconda3
+#Comment out if you've already installed on your system
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh
+chmod a+x Miniconda3-py39_4.9.2-Linux-x86_64.sh
+./Miniconda3-py39_4.9.2-Linux-x86_64.sh
+conda init bash
